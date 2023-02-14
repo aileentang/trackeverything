4c3d61ef2cc124ed0fb1b1cc79858089a9c19c96
aileentang <68557371+aileentang@users.noreply.github.com>
2023-02-14 10:08:38 -0800

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -66,3 +66,192 @@ VALUES
     (4, "Pick Up Tables", "Four tables", "2023/02/13 11:59:59", NULL ), 
     (5, "Look Up Hotels", "Find affordable prices", "2023/02/18 11:59:59", NULL ), 
     (5, "Research Food to Eat", "Ask friends what they recommend", "2023/02/22 11:59:59", NULL );
+--
+-- Table structure for table `Categories`
+--
+
+CREATE OR REPLACE TABLE Categories(
+    categoryID INT(20) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(500),
+    constraint UNIQUE(categoryID),
+    PRIMARY KEY (categoryID)
+);
+
+--
+-- Dumping data for table `Categories`
+--
+
+INSERT INTO Categories ( name, description ) 
+VALUES 
+    ( "project", "Group project for class" ), 
+    ( "quiz", "Graded quizzes" ), 
+    ( "assignment", "Assignments for class" );
+
+--
+-- Table structure for table `Subtasks`
+--
+
+CREATE OR REPLACE TABLE Subtasks(
+    subtaskID INT(20) NOT NULL AUTO_INCREMENT,
+    taskID INT(20) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(500),
+    deadline TIMESTAMP(6) NOT NULL,
+    priorityLevel ENUM("Optional", "Low", "Medium", "High", "Critical"),
+    FOREIGN KEY (taskID) REFERENCES Tasks(taskID)
+    ON DELETE CASCADE,
+    constraint UNIQUE(subtaskID),
+    PRIMARY KEY (subtaskID)
+);
+
+--
+-- Dumping data for table `Subtasks`
+--
+INSERT INTO Subtasks ( taskID, name, description, priorityLevel, deadline ) 
+VALUES 
+    (2, "Read Lecture", "Three lectures to read", "Medium", "2023/03/05 11:59:59" ), 
+    (4, "Buy New Clothes", "clothes need to match", NULL, "2023/03/15 11:59:59" ), 
+    (6, "Check Statement", "Make sure purchases are correct", NULL, "2023/02/10 11:59:59" ), 
+    (8, "Find Resources", "Find links to resources", NULL, "2023/02/09 11:59:59" ), 
+    (10, "Make List", "Write down items needed for burgers", NULL, "2023/02/10 11:59:59" ), 
+    (12, "Check Location of Where to Stay", "Pick central location", NULL, "2023/02/16 11:59:59" );
+
+
+--
+-- Table structure for table `Teammates`
+--
+
+CREATE OR REPLACE TABLE Teammates(
+    teammateID INT(20) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    phone VARCHAR(12),
+    constraint UNIQUE(teammateID),
+    PRIMARY KEY (teammateID)
+);
+
+--
+-- Dumping data for table `Teammates`
+--
+
+INSERT INTO Teammates ( name, email, phone ) 
+VALUES 
+    ( "Jacob", "jacob@gmail.com", "3104356709" ), 
+    ( "John", "john@gmail.com", "6267809120" ), 
+    ( "Ashley", "ashley@gmail.com", "6577083415" ), 
+    ( "Denise", "denise@gmail.com", "7542105639" ), 
+    ( "Jennifer", "jennifer@gmail.com", "6344504000" );
+
+--
+-- Table structure for table `taskAndTeammates`
+--
+
+CREATE OR REPLACE TABLE taskAndTeammates(
+    taskID int,
+    teammateID int,
+    FOREIGN KEY (taskID) REFERENCES Tasks(taskID)
+    ON DELETE CASCADE,
+    FOREIGN KEY (teammateID) REFERENCES Teammates(teammateID)
+    ON DELETE CASCADE,
+    PRIMARY KEY (taskID, teammateID)
+);
+
+--
+-- Dumping data for table `taskAndTeammates`
+--
+
+INSERT INTO taskAndTeammates ( taskID, teammateID ) 
+VALUES 
+    (1, 5), 
+    (2, 2), 
+    (2, 3), 
+    (2, 5);
+
+--
+-- Table structure for table `subtaskAndTeammates`
+--
+
+CREATE OR REPLACE TABLE subtaskAndTeammates(
+    subtaskID int,
+    teammateID int,
+    FOREIGN KEY (subtaskID) REFERENCES Subtasks(subtaskID)
+    ON DELETE CASCADE,
+    FOREIGN KEY (teammateID) REFERENCES Teammates(teammateID)
+    ON DELETE CASCADE,
+    PRIMARY KEY (subtaskID, teammateID)
+);
+
+--
+-- Dumping data for table `subtaskAndTeammates`
+--
+
+INSERT INTO subtaskAndTeammates ( subtaskID, teammateID ) 
+VALUES 
+    (1, 1), 
+    (2, 1), 
+    (3, 1), 
+    (3, 2);
+
+--
+-- Table structure for table `taskAndCategories`
+--
+
+CREATE OR REPLACE TABLE taskAndCategories(
+    taskID int,
+    categoryID int,
+    FOREIGN KEY (taskID) REFERENCES Tasks(taskID)
+    ON DELETE CASCADE,
+    FOREIGN KEY (categoryID) REFERENCES Categories(categoryID)
+    ON DELETE CASCADE,
+    PRIMARY KEY (taskID, categoryID)
+);
+
+--
+-- Dumping data for table `taskAndCategories`
+--
+
+INSERT INTO taskAndCategories ( taskID, categoryID ) 
+VALUES 
+    (1, 1), 
+    (1, 2), 
+    (2, 2), 
+    (3, 2);
+
+--
+-- Table structure for table `subtaskAndCategories`
+--
+
+CREATE OR REPLACE TABLE subtaskAndCategories(
+    subtaskID int,
+    categoryID int,
+    FOREIGN KEY (subtaskID) REFERENCES Subtasks(subtaskID)
+    ON DELETE CASCADE,
+    FOREIGN KEY (categoryID) REFERENCES Categories(categoryID)
+    ON DELETE CASCADE,
+    PRIMARY KEY (subtaskID, categoryID)
+);
+
+--
+-- Dumping data for table `subtaskAndCategories`
+--
+
+INSERT INTO subtaskAndCategories ( subtaskID, categoryID ) 
+VALUES 
+    (1, 1), 
+    (1, 2), 
+    (2, 2), 
+    (3, 2);
+
+SELECT * FROM Users;
+SELECT * FROM Tasks;
+SELECT * FROM Categories;
+SELECT * FROM Subtasks;
+SELECT * FROM Teammates;
+SELECT * FROM taskAndTeammates;
+SELECT * FROM subtaskAndTeammates;
+SELECT * FROM taskAndCategories;
+SELECT * FROM subtaskAndCategories;
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
