74c8b9bbac0026296846829113694af6f82228cf
hoangjennifer <63030751+hoangjennifer@users.noreply.github.com>
2023-02-14 19:26:15 -0800

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -8,8 +8,25 @@ SELECT Users.userID, name, email FROM Users
 SELECT userID, name FROM Users
 
 -- get all task IDs, users, task names, descriptions, deadlines and priority levels for Tasks page
-SELECT Tasks.taskID, Users.name AS userID, Tasks.name, Tasks.description, Tasks.deadline, Tasks.priorityLevel FROM Tasks INNER JOIN Users ON userID = Users.userID
+SELECT Tasks.taskID, Users.name AS userID, name, description, deadline, priorityLevel
+    FROM Tasks
+        INNER JOIN Users
+        ON userID = Users.userID
+
+-- get a single task's data for the Update Task form
+SELECT taskID, userID, name, description, deadline, priorityLevel
+    FROM Tasks
+    WHERE taskID = :task_ID_selected
 
 -- add a new task
 INSERT INTO Tasks (name, description, deadline, priorityLevel) VALUES
     (:nameInput, :descriptionInput, :deadlineInput, :priorityLevelInput)
+
+-- update a task's data
+UPDATE Tasks
+    SET name = :nameInput, description = :descriptionInput,
+        deadline = :deadlineInput, priorityLevel = :priorityLevelInput
+    WHERE taskID = :task_ID_from_update_form
+
+-- delete a task
+DELETE FROM Tasks WHERE taskID = :task_ID_selected
