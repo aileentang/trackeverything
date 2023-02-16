cba9c0a32a577437fbc57f177b42a58dc9d62ccb
aileentang <68557371+aileentang@users.noreply.github.com>
2023-02-15 20:53:10 -0800

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -4,6 +4,9 @@ SET AUTOCOMMIT = 0;
 -- Aileen Tang and Jennifer Hoang
 --
 -- Host: mysql.eecs.oregonstate.edu    Database: `cs340_hoangje`
+-- 
+-- IMPORTANT: Make sure you uncheck 'Enable foreign key checks' if you are copying and pasting the SQL into phpMyAdmin. 
+-- If you don't, it won't import properly! 
 
 --
 -- Table structure for table `Users`
@@ -16,16 +19,6 @@ CREATE OR REPLACE TABLE Users(
     constraint UNIQUE(userID),
     PRIMARY KEY (userID)
 );
-
---
--- Dumping data for table `Users`
---
-
-INSERT INTO Users (name, email) VALUES 
-    ("Momo", "babygirlmomo@gmail.com"),
-    ("Aileen", "tangai@oregonstate.edu"),
-    ("Catherine", "catherine@hello.com"),
-    ("Jennifer", NULL);
 
 --
 -- Table structure for table `Tasks`
@@ -45,40 +38,6 @@ CREATE OR REPLACE TABLE Tasks(
 );
 
 --
--- Dumping data for table `Tasks`
---
-
-INSERT INTO Tasks (userID, name, description, deadline, priorityLevel) VALUES 
-    (2, "Project Draft 1", "Step 1 draft", "2023-03-07 11:59:59", "Low"),
-    (2, "Quiz 1", "Relational DB and design", "2023-03-13 11:59:59", "Medium"),
-    (1, "Beach Walk", "Get some sandy paws", "2023-03-20 11:59:59", "Low"),
-    (3, "Chase Sapphire Bill", "Pay credit card bill", "2023-02-15 11:59:59", NULL);
---
--- Table structure for table `Categories`
---
-
-CREATE OR REPLACE TABLE Categories(
-    categoryID INT(20) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    description VARCHAR(500),
-    userID INT(20) NOT NULL,
-    constraint UNIQUE(categoryID),
-    FOREIGN KEY (userID) REFERENCES Users(userID)
-    ON DELETE CASCADE,
-    PRIMARY KEY (categoryID)
-);
-
---
--- Dumping data for table `Categories`
---
-
-INSERT INTO Categories (name, description) VALUES 
-    ("project", "Group project for class"), 
-    ("quiz", "Graded quizzes"), 
-    ("finance", "Financial management"),
-    ("events", NULL);
-
---
 -- Table structure for table `Subtasks`
 --
 
@@ -94,15 +53,6 @@ CREATE OR REPLACE TABLE Subtasks(
     constraint UNIQUE(subtaskID),
     PRIMARY KEY (subtaskID)
 );
-
---
--- Dumping data for table `Subtasks`
---
-INSERT INTO Subtasks (taskID, name, description, priorityLevel, deadline) VALUES 
-    (2, "Read module 1", NULL, "Medium", "2023-03-05 11:59:59"), 
-    (4, "Check statement", "Make sure purchases are correct", NULL, "2023-02-10 11:59:59"),  
-    (2, "Read module 2", NULL, NULL, "2023-03-06 11:59:59");
-
 
 --
 -- Table structure for table `Teammates`
@@ -121,13 +71,19 @@ CREATE OR REPLACE TABLE Teammates(
 );
 
 --
--- Dumping data for table `Teammates`
+-- Table structure for table `Categories`
 --
 
-INSERT INTO Teammates (userID, name, email, phone) VALUES 
-    (4, "Jacob", "jacob@gmail.com", "3104356709"), 
-    (2, "John", "john@gmail.com", "6267809120"), 
-    (3, "Ashley", "ashley@gmail.com", "6577083415");
+CREATE OR REPLACE TABLE Categories(
+    categoryID INT(20) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(500),
+    userID INT(20) NOT NULL,
+    constraint UNIQUE(categoryID),
+    FOREIGN KEY (userID) REFERENCES Users(userID)
+    ON DELETE CASCADE,
+    PRIMARY KEY (categoryID)
+);
 
 --
 -- Table structure for table `TaskAndTeammates`
@@ -144,15 +100,6 @@ CREATE OR REPLACE TABLE TaskAndTeammates(
 );
 
 --
--- Dumping data for table `TaskAndTeammates`
---
-
-INSERT INTO TaskAndTeammates (taskID, teammateID) VALUES 
-    (1, 2), 
-    (2, 2), 
-    (2, 3);
-
---
 -- Table structure for table `SubtaskAndTeammates`
 --
 
@@ -165,15 +112,6 @@ CREATE OR REPLACE TABLE SubtaskAndTeammates(
     ON DELETE CASCADE,
     PRIMARY KEY (subtaskID, teammateID)
 );
-
---
--- Dumping data for table `SubtaskAndTeammates`
---
-
-INSERT INTO SubtaskAndTeammates (subtaskID, teammateID) VALUES 
-    (1, 3), 
-    (3, 3), 
-    (3, 2);
 
 --
 -- Table structure for table `TaskAndCategories`
@@ -190,16 +128,6 @@ CREATE OR REPLACE TABLE TaskAndCategories(
 );
 
 --
--- Dumping data for table `TaskAndCategories`
---
-
-INSERT INTO TaskAndCategories (taskID, categoryID) VALUES 
-    (1, 1), 
-    (2, 2), 
-    (3, 4), 
-    (4, 3);
-
---
 -- Table structure for table `SubtaskAndCategories`
 --
 
@@ -213,10 +141,87 @@ CREATE OR REPLACE TABLE SubtaskAndCategories(
     PRIMARY KEY (subtaskID, categoryID)
 );
 
+---------------
+-- INSERTIONS--
+---------------
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO Users (name, email) VALUES 
+    ("Momo", "babygirlmomo@gmail.com"),
+    ("Aileen", "tangai@oregonstate.edu"),
+    ("Catherine", "catherine@hello.com"),
+    ("Jennifer", NULL);
+    
+--
+-- Dumping data for table `Tasks`
+--
+
+INSERT INTO Tasks (userID, name, description, deadline, priorityLevel) VALUES 
+    (2, "Project Draft 1", "Step 1 draft", "2023-03-07 11:59:59", "Low"),
+    (2, "Quiz 1", "Relational DB and design", "2023-03-13 11:59:59", "Medium"),
+    (1, "Beach Walk", "Get some sandy paws", "2023-03-20 11:59:59", "Low"),
+    (3, "Chase Sapphire Bill", "Pay credit card bill", "2023-02-15 11:59:59", NULL);
+
+--
+-- Dumping data for table `Subtasks`
+--
+INSERT INTO Subtasks (taskID, name, description, priorityLevel, deadline) VALUES 
+    (2, "Read module 1", NULL, "Medium", "2023-03-05 11:59:59"), 
+    (4, "Check statement", "Make sure purchases are correct", NULL, "2023-02-10 11:59:59"),  
+    (2, "Read module 2", NULL, NULL, "2023-03-06 11:59:59");
+
+--
+-- Dumping data for table `Teammates`
+--
+
+INSERT INTO Teammates (userID, name, email, phone) VALUES 
+    (4, "Jacob", "jacob@gmail.com", "3104356709"), 
+    (2, "John", "john@gmail.com", "6267809120"), 
+    (3, "Ashley", "ashley@gmail.com", "6577083415");
+    
+--
+-- Dumping data for table `Categories`
+--
+INSERT INTO Categories (name, description) VALUES 
+    ("project", "Group project for class"), 
+    ("quiz", "Graded quizzes"), 
+    ("finance", "Financial management"),
+    ("events", NULL);
+
+--
+-- Dumping data for table `TaskAndTeammates`
+--
+
+INSERT INTO TaskAndTeammates (taskID, teammateID) VALUES 
+    (1, 2), 
+    (2, 2), 
+    (2, 3);
+
+--
+-- Dumping data for table `SubtaskAndTeammates`
+--
+
+INSERT INTO SubtaskAndTeammates (subtaskID, teammateID) VALUES 
+    (1, 3), 
+    (3, 3), 
+    (3, 2);
+
+--
+-- Dumping data for table `TaskAndCategories`
+--
+
+INSERT INTO TaskAndCategories (taskID, categoryID) VALUES 
+    (1, 1), 
+    (2, 2), 
+    (3, 4), 
+    (4, 3);
+
 --
 -- Dumping data for table `SubtaskAndCategories`
 --
-
 INSERT INTO SubtaskAndCategories ( subtaskID, categoryID ) VALUES 
     (1, 1), 
     (2, 4), 
