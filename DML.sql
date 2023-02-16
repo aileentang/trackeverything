71dc1f428120eeb94ee5eae8e66265e9fd1606f8
hoangjennifer <63030751+hoangjennifer@users.noreply.github.com>
2023-02-15 22:44:37 -0800

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -10,12 +10,44 @@ INNER JOIN Users ON Tasks.userID = Users.userID
 ORDER BY taskID;
 
 -- get a single task's data for the Update Task form
-SELECT taskID, userID, name, description, deadline, priorityLevel FROM Tasks
+SELECT taskID, Users.userID, name, description, deadline, priorityLevel FROM Tasks
 WHERE taskID = :task_ID_selected;
+
+-- get all user data for List Users page
+SELECT userID, name, email FROM Users;
+
+-- get a single user's data for the Update User form
+SELECT userID, name, email FROM Users
+WHERE userID = :user_ID_selected;
+
+-- get all category data for List Categories page
+SELECT categoryID, Users.name AS userID, Categories.name, description FROM Categories
+INNER JOIN Users ON Categories.userID = Users.userID
+ORDER BY categoryID;
+
+-- get all subtask data for List Subtasks page
+SELECT subtaskID, taskID, name, description, deadline, priorityLevel FROM Subtasks;
+
+-- get all teammate data for List Teammates page
+SELECT teammateID, Users.name AS userID, Teammates.name, Teammates.email, phone FROM Teammates
+INNER JOIN Users ON Teammates.userID = Users.userID
+ORDER BY teammateID;
 
 -- add a new task
 INSERT INTO Tasks (name, description, deadline, priorityLevel) VALUES
     (:nameInput, :descriptionInput, :deadlineInput, :priorityLevelInput);
+
+-- add a new user
+INSERT INTO Users (name, email) VALUES
+    (:nameInput, :emailInput);
+
+-- add a new category
+INSERT INTO Categories (userID, name, description) VALUES
+    (:userIDInput, :nameInput, :descriptionInput);
+
+-- add a new subtask
+INSERT INTO Subtasks (taskID, name, description, deadline, priorityLevel) VALUES
+    (:taskIDInput, :nameInput, :descriptionInput, :deadlineInput, :priorityLevelInput);
 
 -- update a task's data
 UPDATE Tasks
@@ -25,3 +57,12 @@ UPDATE Tasks
 
 -- delete a task
 DELETE FROM Tasks WHERE taskID = :task_ID_selected;
+
+-- delete a user
+DELETE FROM Users WHERE userID = :user_ID_selected;
+
+-- delete a category
+DELETE FROM Categories WHERE categoryID = :category_ID_selected;
+
+-- delete a subtask
+DELETE FROM Subtasks WHERE subtaskID = :subtask_ID_selected;
