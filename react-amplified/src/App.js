3df6f6b1c338d6c7e338fa68d3a0c9c66012b6d7
Aileen Tang <aileentang@Nashifs-MBP.lan>
2023-03-01 03:56:10 -0800

    working deployment:)

--- react-amplified/src/App.js
+++ react-amplified/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Jennifer!!! :D - Aileen
         </p>
         <a
           className="App-link"
